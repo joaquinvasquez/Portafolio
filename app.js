e6e118605ec696f5857216598d63e4cc4a6241e4
Joaquin <joaquinvasquez20@gmail.com>
2022-02-21 02:36:13 -0300

    cambie el header a flexbox y agregue el link a llamada de celular en footer

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
-document.getElementById('switch').addEventListener('click', () => {
-    document.body.classList.toggle('dark')
+document.getElementById("switch").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 
-    if(document.body.classList.contains('dark')){
-        document.getElementById('cambio-boton').classList.replace('izq', 'der')
-    }else{
-        document.getElementById('cambio-boton').classList.replace('der', 'izq')
-    }
+  if (document.body.classList.contains("dark")) {
+    document.getElementById("cambio-boton").classList.replace("izq", "der");
+  } else {
+    document.getElementById("cambio-boton").classList.replace("der", "izq");
+  }
 
-    document.getElementById('info').classList.toggle('info-dark')
+  document.getElementById("info").classList.toggle("info-dark");
 
-    if(document.body.classList.contains('dark')){
-        document.getElementById('volver').classList.replace('btn-volver', 'btn-volver-dark')
-    }else{
-        document.getElementById('volver').classList.replace('btn-volver-dark', 'btn-volver')
-    }
+  if (document.body.classList.contains("dark")) {
+    document
+      .getElementById("volver")
+      .classList.replace("btn-volver", "btn-volver-dark");
+  } else {
+    document
+      .getElementById("volver")
+      .classList.replace("btn-volver-dark", "btn-volver");
+  }
 
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark-mode", "true");
+  } else {
+    localStorage.setItem("dark-mode", "false");
+  }
+});
 
-    if(document.body.classList.contains('dark')){
-		localStorage.setItem('dark-mode', 'true')
-	}else{
-		localStorage.setItem('dark-mode', 'false')
-	}
-})
+cargaTema = function () {
+  if (localStorage.getItem("dark-mode") === "true") {
+    document.body.classList.add("dark");
+    document.getElementById("cambio-boton").classList.replace("izq", "der");
+    document.getElementById("info").classList.add("info-dark");
+    document
+      .getElementById("volver")
+      .classList.replace("btn-volver", "btn-volver-dark");
+  } else {
+    document.body.classList.remove("dark");
+    document.getElementById("cambio-boton").classList.replace("der", "izq");
+    document.getElementById("info").classList.remove("info-dark");
+    document
+      .getElementById("volver")
+      .classList.replace("btn-volver-dark", "btn-volver");
+  }
+};
 
-cargaTema = function(){
-    if(localStorage.getItem('dark-mode') === 'true'){
-        document.body.classList.add('dark')
-        document.getElementById('cambio-boton').classList.replace('izq', 'der')
-    document.getElementById('info').classList.add('info-dark')
-    document.getElementById('volver').classList.replace('btn-volver', 'btn-volver-dark')
-    }else{
-        document.body.classList.remove('dark')
-        document.getElementById('cambio-boton').classList.replace('der', 'izq')
-    document.getElementById('info').classList.remove('info-dark')
-    document.getElementById('volver').classList.replace('btn-volver-dark', 'btn-volver')
-    }
-}
-
+document.getElementById("dropdown").addEventListener("click", () => {
+  document.querySelector(".links").classList.toggle('links-block');
+});
